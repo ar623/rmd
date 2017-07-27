600718ea7d4ca11399469096560b32a6b1b45730
jerry <jb7959@naver.com>
2017-07-27 10:50:25 +0900

    interactive chart 추가

--- target/classes/Static/Dist/js/common.js
+++ target/classes/Static/Dist/js/common.js
@@ -1,5 +1,3 @@
-(function ($) {
-
 //첫번째 계기판
     $('#rmd1').kumaGauge({
         value: Math.floor((Math.random() * 99) + 1)
@@ -11,7 +9,7 @@
     });
 
 //계기판 값 업데이트트
-    function rmdUpdating() {
+   function rmdUpdating() {
         $('#rmd1').kumaGauge('update', {
             value: Math.floor((Math.random() * 99) + 1)
         });
@@ -19,8 +17,7 @@
         $('#rmd2').kumaGauge('update', {
             value: Math.floor((Math.random() * 99) + 1)
         });
-        alert("H!")
-    }
+    };
 
     $('.js-gauge--2').kumaGauge({
         value: Math.floor((Math.random() * 99) + 1),
@@ -38,14 +35,4 @@
             fontWeight: 'bold'
         }
     });
-    /*var update = setInterval(function () {
-        $.getScript("/Dist/js/kuma-gauge.jquery.js", function () {
-
-            $('#rmd1').kumaGauge('update', {
-                value: Math.floor((Math.random() * 99) + 1)
-            });
-            console.log("동작중");
-        });
-    }, 3000);*/
-
-})(jQuery);
+    setInterval(rmdUpdating, 3000);
